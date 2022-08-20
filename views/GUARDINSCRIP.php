ebd072460ba1d3534c5019132d25e579f3172c39
RickGrimmes <babajasgo@hotmail.com>
2022-08-20 18:50:30 -0500

    sigo en inscripcion

--- views/GUARDINSCRIP.php
+++ views/GUARDINSCRIP.php
@@ -11,15 +11,11 @@
     <div class="container"> 
         <?php
         use MyApp\Query\ejecuta;
-        use MyApp\Query\Select; 
         require("../vendor/autoload.php");
-        $usuario = new Seleccionar();
         extract($_POST);
-        $user= new ejecutar();
         extract($_POST);
-        $usfk="SELECT USUARIO.ID_US FROM USUARIO INNER JOIN INSCRIP_GYM ON USUARIO.ID_US=INSCRIP_GYM.USER_FK";
+        $usfk='USER_FK';
         $cadena="INSERT INTO INSCRIP_GYM (TEL_EMERGENCIA, T_PAGO, FECHA_INSCRIP, USER_FK) VALUES('$telefono','$metodo','$fecha','$telefono','$usfk')";
-        $usuario->seleccionar($usfk);
         $user->ejecutar($cadena);
         echo "<div class='alert alert=success'>CLIENTE INSCRITO</div>";
         header("refresh:40 loginapp.php");
